cccde2323dd265902e279be05c5ef6ac56bcf558
“ADRIENNE <adrienneklee@gmail.com>
2022-11-03 15:45:31 -0400

    modal and bootstrap added

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,28 +1,44 @@
-// Importing JavaScript
-//
-// You have two choices for including Bootstrap's JS files—the whole thing,
-// or just the bits that you need.
-
-
-// Option 1
-//
-// Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
-
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
-// Option 2
-//
-// Import just what we need
-
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/popper.js/dist/popper.min.js";
 import "../../node_modules/bootstrap/js/dist/util.js";
 import "../../node_modules/bootstrap/js/dist/modal.js";
 
-import { createWidget } from '@typeform/embed'
-import '@typeform/embed/build/css/widget.css'
+// import { createWidget } from '@typeform/embed';
+// import '@typeform/embed/build/css/widget.css';
+
+// createWidget('<form-id>', options);
 
 
-createWidget('<form-id>', options)
+$(document).ready(function() {
+
+// Gets the video src from the data-src on each button
+
+var $videoSrc;  
+$('.video-btn').click(function() {
+    $videoSrc = $(this).data( "src" );
+});
+console.log($videoSrc);
+
+  
+  
+// when the modal is opened autoplay it  
+$('#myModal').on('shown.bs.modal', function (e) {
+    
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+
+})
+  
+
+
+// stop playing the youtube video when I close the modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src',$videoSrc); 
+}) 
+// document ready  
+});
+
+
+
+
